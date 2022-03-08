e4711b099824663a62221173bdcb1246cb37a179
AnVynn <an.vynnyk@gmail.com>
2022-03-08 21:04:29 +0200

    update OpenApiConfig

--- src/main/java/com/example/restaurantvoting/config/OpenApiConfig.java
+++ src/main/java/com/example/restaurantvoting/config/OpenApiConfig.java
@@ -21,8 +21,8 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(
                 title = "REST API documentation",
                 version = "1.0",
-                description = "Приложение по <a href='https://javaops.ru/view/topjava2'>курсу TopJava-2</a> (решение выпускного проекта)",
-                contact = @Contact(url = "https://javaops.ru/#contacts", name = "Grigory Kislin", email = "admin@javaops.ru")
+                description = "Application by <a href='https://javaops.ru/view/topjava2'>course TopJava-2</a> (graduation project solution)",
+                contact = @Contact(name = "Anastasiia Vynnyk", email = "an.vynnyk@gmail.com")
         ),
         security = @SecurityRequirement(name = "basicAuth")
 )
